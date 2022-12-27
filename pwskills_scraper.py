ae96d669909c464447c83dce3d6aefbf54430ef2
Anshul Raj Verma <arv.anshul.1864@gmail.com>
2022-12-27 12:14:12 +0530

    Added 2 fetch functions

--- pwskills_scraper.py
+++ pwskills_scraper.py
@@ -1,3 +1,5 @@
+""" Scraper for PWSkills courses to make a DataFrame. """
+
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
@@ -5,11 +7,16 @@ import json
 from typing import Literal
 
 URLS = ['https://pwskills.com/course/Data-Science-masters',
-        'https://pwskills.com/course/Cracking-the-Coding-Interview-in-Java---Foundation',
+        'https://pwskills.com/course/Java-with-DSA-and-system-design',
         'https://pwskills.com/course/Full-Stack-web-development']
 
 
 def scrape_pwskills(url: str, what: Literal['curriculum', 'projects']) -> pd.DataFrame:
+    """
+    Scraper for PW SKills `paid courses`.
+
+    Returns the DataFrame which contains the `['title', 'date', 'parts', 'n_parts']` columns.
+    """
     page = requests.get(url).content
     soup = BeautifulSoup(page, 'html.parser')
 
@@ -21,7 +28,7 @@ def scrape_pwskills(url: str, what: Literal['curriculum', 'projects']) -> pd.Dat
 
     df['parts'] = df['items'].apply(
         lambda x: [i['title'] for i in x])  # type: ignore
-    df['n_parts'] = df['parts'].apply(lambda x: len(x))
+    df['n_parts'] = df['parts'].apply(len)
 
     def extract_date(x: str) -> str:
         x = x.replace("'23", ' 2023').replace("' 23", ' 2023')
@@ -31,7 +38,7 @@ def scrape_pwskills(url: str, what: Literal['curriculum', 'projects']) -> pd.Dat
     df['date'] = df['title'].apply(extract_date)
 
     def filter_title(x: str) -> str:
-        x = x.replace("' 23", "'23")
+        x = x.replace("' 23", "'23").replace(" '23", "'23")
         return ' '.join(x.split()[2:])
 
     df['title'] = df['title'].apply(filter_title)
@@ -45,6 +52,7 @@ def scrape_pwskills(url: str, what: Literal['curriculum', 'projects']) -> pd.Dat
 
 
 def enhance_my_data(data: pd.DataFrame) -> pd.DataFrame:
+    """ Enhances the given `paid course` DataFrame. """
     title_grp = data.groupby('title')
     df = title_grp[['parts', 'n_parts']].sum(False).reset_index()
 
@@ -60,3 +68,31 @@ def enhance_my_data(data: pd.DataFrame) -> pd.DataFrame:
     df.drop(columns=['index'], inplace=True)
 
     return df
+
+
+def fetch_instructor(url: str) -> list[str]:
+    """ Fetches instructors names from the given course url. """
+    page = requests.get(url).content
+    soup = BeautifulSoup(page, 'html.parser')
+
+    tag = soup.find_all('script')
+    js = json.loads(tag[20].get_text())
+
+    program_instructors = js['props']['pageProps']['data']['meta']['instructors']
+    instructors = js['props']['pageProps']['initialState']['init']['instructors']
+
+    instructors_name: list[str] = [instructors[i]['name']
+                                   for i in program_instructors]
+    return instructors_name
+
+
+def fetch_overview(url: str) -> list[list[str]]:
+    """ Fetches learnning and features overview from the given course url. """
+    page = requests.get(url).content
+    soup = BeautifulSoup(page, 'html.parser')
+
+    tag = soup.find_all('script')
+    js = json.loads(tag[20].get_text())
+    overview = js['props']['pageProps']['data']['meta']['overview']
+
+    return [overview['learn'], overview['features']]
